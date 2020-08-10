6a4d3190d8c13d4f4354d78fc66c49d04310fa21
yuruOvO <30980083+yururyuru00@users.noreply.github.com>
2020-08-10 19:10:28 +0900

    Add files via upload

--- utilities.py
+++ utilities.py
@@ -1,9 +1,5 @@
-import datetime
 import numpy as np
-import matplotlib.pyplot as plt
 import math
-from matplotlib import cm
-from sklearn.metrics import silhouette_samples
 
 from Parameters import P
 import MatLib as ml
@@ -48,14 +44,14 @@ def make_sp1(features):
         return mat
             
     usr_size, dim_size = len(features), len(features[0])
-    if(P.AN_type == 'h'): #AN_type=human
+    if(P.dataset_type == 'social_net'): #AN_type=human
         mats = [np.zeros((usr_size, usr_size)) for i in range(dim_size)]
         for i, feature in enumerate(features.T):
             mats[i] = makeMat_sp1_i(feature, usr_size)
             ml.normalized(mats[i])
         return mats
     
-    else: #AN_type=document
+    elif(P.dataset_type == 'citation_net'):
         mat = np.zeros((usr_size, usr_size))
         for i in range(usr_size):
             for j in range(i+1, usr_size):
@@ -78,17 +74,17 @@ class Graph():
         self.N = np.array([set() for i in range(usr_size)])
         for opponent, obj in edges:
             self.N[obj].add(opponent)
-        self.max_size = np.zeros(P.sita+1)
-        self.R = np.array([[set() for i in range(usr_size)] for j in range(P.sita+1)])
-        self.Ft = np.array([[set() for i in range(usr_size)] for j in range(P.sita+1)])    
+        self.max_size = np.zeros(P.theta+1)
+        self.R = np.array([[set() for i in range(usr_size)] for j in range(P.theta+1)])
+        self.Ft = np.array([[set() for i in range(usr_size)] for j in range(P.theta+1)])    
     
     def makeMat_sp2(self):
         s2a = np.diag([0. for i in range(len(self.N))])
         s2b = np.diag([0. for i in range(len(self.N))])
         for i in range(len(s2a)):
             for j in self.N[i]:
-                s2a[i][j] = P.gamma
-                s2b[i][j] = P.gamma
+                s2a[i][j] = P.delta
+                s2b[i][j] = P.delta
             self.max_size[1] = max(self.max_size[1], len(self.N[i]))
         for v in range(len(self.N)):
             self.R[0][v].add(v)
@@ -96,7 +92,7 @@ class Graph():
             self.R[1][v].add(v)
             self.R[1][v] = self.R[1][v] | self.N[v]
             self.Ft[1][v] = self.N[v]
-        for step in range(2, P.sita+1):
+        for step in range(2, P.theta+1):
             for v in range(len(self.N)):
                 self.R[step][v] = self.R[step-1][v].copy()
                 for n in self.N[v]:
@@ -117,37 +113,8 @@ class Graph():
         else:
             rate1 = (path-1) / (len(self.Ft[step-1][v])-1)
         rate2 = (path-1) / self.max_size[step-1]
-        delta = math.pow(P.gamma, step-1) - math.pow(P.gamma, step)
-        return math.pow(P.gamma, step) + delta * rate1,                math.pow(P.gamma, step) + delta * rate2
-
-def plot_silhouette_score(X, pred_clusters, iterate):
-    cluster_labels = np.unique(pred_clusters)
-    n_clusters=cluster_labels.shape[0]
-    if(P.AN_type=='h'):
-        silhouette_vals = silhouette_samples(X, pred_clusters, metric='euclidean')
-    else: 
-        silhouette_vals = silhouette_samples(X, pred_clusters, metric='cosine')
-    y_ax_lower, y_ax_upper, yticks= 0, 0, []
-
-    fig = plt.figure(figsize=(17, 30))
-    for i,c in enumerate(cluster_labels):
-        c_silhouette_vals = silhouette_vals[pred_clusters==c]
-        c_silhouette_vals.sort()
-        y_ax_upper += len(c_silhouette_vals)
-        color = cm.jet(float(i)/n_clusters)
-        plt.barh(range(y_ax_lower,y_ax_upper),
-                         c_silhouette_vals,
-                         height=1.0,
-                         edgecolor='none',
-                         color=color)
-        yticks.append((y_ax_lower+y_ax_upper)/2)
-        y_ax_lower += len(c_silhouette_vals)
-    silhouette_avg = np.mean(silhouette_vals)
-    plt.axvline(silhouette_avg,color="red",linestyle="--")
-    plt.yticks(yticks,cluster_labels + 1)
-    plt.ylabel('Cluster')
-    plt.xlabel('silhouette coefficient')
-    fig.savefig('./experiment/silhouette_{}_{}_{}.png'.format(P.AN_data, P.mode, iterate))
+        delta = math.pow(P.delta, step-1) - math.pow(P.delta, step)
+        return math.pow(P.delta, step) + delta * rate1, math.pow(P.delta, step) + delta * rate2
 
 def purity(ans, estimated):
     usr_size = len(estimated)
