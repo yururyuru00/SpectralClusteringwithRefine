3ad1ff360cb0d502623f39b0660cbd3f8715abc4
yuruOvO <jima971123@gmail.com>
2020-10-16 11:56:49 +0900

    add stopping criteria

--- SClump.py
+++ SClump.py
@@ -1,6 +1,7 @@
 import argparse
 import numpy as np
-import cvxopt, scipy.optimize
+import cvxopt
+import scipy.optimize
 from cvxopt import matrix
 from scipy.sparse.linalg import eigsh
 from sklearn.cluster import KMeans
@@ -12,23 +13,25 @@ from Parameters import P
 
 
 def spectral_clustering(S, labels, iter):
-    if(P.disable_normalization == False): Ls = ml.makeNormLaplacian(S)
-    else: Ls = ml.makeLaplacian(S)
-    try: 
-        eigen_val, eigen_vec = eigsh(Ls, P.cluster_size, which="SM")
-    except scipy.sparse.linalg.ArpackNoConvergence:
-        return 'nonconverge', -1, -1, -1
-    
-    metrics = {'ari':0., 'nmi':0., 'purity':0.}
+    if(P.disable_normalization == False):
+        Ls = ml.makeNormLaplacian(S)
+    else:
+        Ls = ml.makeLaplacian(S)
+    eigen_val, eigen_vec = eigsh(Ls, P.cluster_size, which="SM")
+
+    metrics = {'ari': 0., 'nmi': 0., 'purity': 0.}
     for _ in range(10):
         k_means = KMeans(n_clusters=P.cluster_size, n_init=10, tol=0.0000001)
         k_means.fit(eigen_vec)
         metrics['ari'] += clus.adjusted_rand_score(labels, k_means.labels_)
-        metrics['nmi'] += clus.adjusted_mutual_info_score(labels, k_means.labels_, "arithmetic")
+        metrics['nmi'] += clus.adjusted_mutual_info_score(
+            labels, k_means.labels_, "arithmetic")
         metrics['purity'] += utilities.purity(labels, k_means.labels_)
-    for key in metrics.keys(): metrics[key] = metrics[key] / 10.
-    np.savetxt('./result/{}/pred_unnorm_{}.csv'.format(P.dataset, iter), k_means.labels_)
-    return 'converge', eigen_val, eigen_vec, metrics
+    for key in metrics.keys():
+        metrics[key] = metrics[key] / 10.
+
+    return k_means.labels_, eigen_val, eigen_vec, metrics
+
 
 def ini_Q(sp):
     obj_size, dim_size = len(sp[0]), len(sp)
@@ -45,21 +48,24 @@ def ini_Q(sp):
         Q[d][d] += P.beta
     return Q
 
+
 def ini_lambda(sp):
     dim_size = len(sp)
     if(P.dataset_type == 'social_net'):
         lamb = np.zeros(dim_size)
         with open('./data/{0}/{0}_pca_contribution_ratio.csv'.format(P.dataset), 'r') as r:
             pca_contribution_ratio = r.readline().rstrip().split(' ')
-            pca_contribution_ratio = np.array([float(val) for val in pca_contribution_ratio])
+            pca_contribution_ratio = np.array(
+                [float(val) for val in pca_contribution_ratio])
         ratio_sum = np.sum(pca_contribution_ratio)
         for i in range(dim_size-2):
             lamb[i] = 0.5 * (pca_contribution_ratio[i]/ratio_sum)
         lamb[dim_size-2], lamb[dim_size-1] = 0.25, 0.25
         return lamb
     elif(P.dataset_type == 'citation_net'):
-        lamb = np.array([0.5,0.25,0.25])
+        lamb = np.array([0.5, 0.25, 0.25])
         return lamb
+
 
 def ini_W(sp, lamb):
     obj_size, dim_size = len(sp[0]), len(sp)
@@ -67,14 +73,15 @@ def ini_W(sp, lamb):
     for i in range(dim_size):
         W += sp[i]*lamb[i]
     return W
-    
+
+
 def update_S(S, W, vec):
     def func_lamb(x):
         sum_ = 0.0
         for j in range(len(ui)):
             sum_ += ml.relu(x - ui[j])
         return sum_/P.number_of_updates - x
-        
+
     def func_prime_lamb(x):
         sum_ = 0.0
         for j in range(len(ui)):
@@ -86,13 +93,15 @@ def update_S(S, W, vec):
     for i in range(len(S)):
         arg = np.argsort(-S[i])
         tops = np.array([arg[j] for j in range(P.number_of_updates)])
-        pi = np.array([(2.*W[i][top] - P.gamma*ml.l2norm(vec[i]-vec[top])*ml.l2norm(vec[i]-vec[top])) 
-                        / ((2.+2.*P.alpha)) for top in tops])
-        ui = pi + 1./P.number_of_updates*v_1 - np.dot(v_1, pi)/P.number_of_updates*v_1
+        pi = np.array([(2.*W[i][top] - P.gamma*ml.l2norm(vec[i]-vec[top])*ml.l2norm(vec[i]-vec[top]))
+                       / ((2.+2.*P.alpha)) for top in tops])
+        ui = pi + 1./P.number_of_updates*v_1 - \
+            np.dot(v_1, pi)/P.number_of_updates*v_1
         opt_lamb = scipy.optimize.newton(func_lamb, 0., func_prime_lamb)
         S[i] = np.zeros(len(S[i]))
         for j in range(P.number_of_updates):
             S[i][tops[j]] = ml.relu(ui[j]-opt_lamb)
+
 
 def update_lamb(lamb, S, sp, Q):
     Q = matrix(Q)
@@ -103,69 +112,94 @@ def update_lamb(lamb, S, sp, Q):
     b = matrix(np.array([1.]))
 
     cvxopt.solvers.options['show_progress'] = False
-    sol=cvxopt.solvers.qp(Q,q,G,h,A,b)
+    sol = cvxopt.solvers.qp(Q, q, G, h, A, b)
     index = 0
     for val in sol["x"]:
         lamb[index] = val
         index += 1
-        
+
+
 def gamma_tuning(eigen_values):
     number_of_eigen0 = 0
     for eig in eigen_values:
-        if(eig < 1E-10): number_of_eigen0 += 1
+        if(eig < 1E-10):
+            number_of_eigen0 += 1
     if(number_of_eigen0 < P.cluster_size):
         P.gamma += (-(number_of_eigen0-1.)/(P.cluster_size-1.) + 1.) * P.gamma
     elif(number_of_eigen0 > P.cluster_size):
         P.gamma = P.gamma / 2.
-    print("\tnum of components: {}\n".format(number_of_eigen0))
-    
+
     return number_of_eigen0
+
 
 def main():
     print('\ninitialize start ...\n')
-    #set the hyperparameters
+    # set the hyperparameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('dataset', type=str, help='any of the following datasets {cora, citeseer, football, politicsuk, football}')
+    parser.add_argument(
+        'dataset', type=str, help='any of the following datasets {cora, citeseer, football, politicsuk, olympics}')
     parser.add_argument('c', type=int, help='number of clusters')
-    parser.add_argument('--sigma', default=3., type=float, 
-                                    help='parameter sigma used to generate similarity matrix S_A')
-    parser.add_argument('--theta', default=3, type=int, 
-                                    help='parameter theta used to generate similarity matrix S_L')
-    parser.add_argument('--delta', default=0.6, type=float, 
-                                    help='parameter theta used to generate similarity matrix S_L')
-    parser.add_argument('--m', default=80, type=int, 
-                                    help='parameter to determine how many elements in similarity matrices to update')
+    parser.add_argument('--sigma', default=3., type=float,
+                        help='parameter sigma used to generate similarity matrix S_A')
+    parser.add_argument('--theta', default=3, type=int,
+                        help='parameter theta used to generate similarity matrix S_L')
+    parser.add_argument('--delta', default=0.6, type=float,
+                        help='parameter theta used to generate similarity matrix S_L')
+    parser.add_argument('--m', default=80, type=int,
+                        help='parameter to determine how many elements in similarity matrices to update')
     parser.add_argument('--disable', action='store_true', default=False,
-                                    help='Disables the normalization process of Laplacian matrices')
+                        help='Disables the normalization process of Laplacian matrices')
     setting = parser.parse_args()
-    P.set(setting) #set the parameters to Parameter Class P 
+    P.set(setting)  # set the parameters to Parameter Class P
 
-    #initialize
+    # initialize
     np.random.seed(0)
-    if(P.dataset=='cora'): features, edges, labels = utilities.load_fromgen(P.dataset)
-    else: features, edges, labels = utilities.load_fromcsv(P.dataset)
+    if(P.dataset == 'cora'):
+        features, edges, labels = utilities.load_fromgen(P.dataset)
+    else:
+        features, edges, labels = utilities.load_fromcsv(P.dataset)
     sp1 = utilities.make_sp1(features)
     sp2a, sp2b = utilities.make_sp2(edges, len(sp1[0]))
     sp = sp1 + [sp2a, sp2b]
-    Q = ini_Q(sp) #matrix Q is used for later process(update_lambda)
+    Q = ini_Q(sp)  # matrix Q is used for later process(update_lambda)
     lamb = ini_lambda(sp)
     W = ini_W(sp, lamb)
     S = np.copy(W)
 
-    #Spectral Clustering + Refine(update_S)
+    # Spectral Clustering + Refine(update S, lambda, W)
+    mean_silhouette_score = np.zeros(100)
     for tri in range(100):
-        state, eigen_val, eigen_vec, metrics = spectral_clustering(S, labels, tri)
-        if(state == 'converge'):
-            print("tri: {}\n\tARI: {:.4f}\n\tNMI: {:.4f}\n\tPurity: {:.4f}" \
-                        .format(tri, metrics['ari'], metrics['nmi'], metrics['purity']))
-            number_of_eigen0 = gamma_tuning(eigen_val)
-            if(number_of_eigen0 == P.cluster_size): break
-        else: print("=========\narpack error\n========="); break
+        pred_clusters, eigen_val, eigen_vec, metrics = spectral_clustering(
+            S, labels, tri)
 
+        print("tri: {}\n\tARI: {:.4f}\n\tNMI: {:.4f}\n\tPurity: {:.4f}"
+              .format(tri, metrics['ari'], metrics['nmi'], metrics['purity']))
+        number_of_eigen0 = gamma_tuning(eigen_val)
+        print("\tnum of components: {}".format(number_of_eigen0))
+
+        # stopping criteria
+        mean_silhouette_score[tri] = utilities.calc_silhouette_score(
+            P.dataset, pred_clusters)
+        print('\tmean silhouette score : {}\n'.format(
+            mean_silhouette_score[tri]))
+        if(tri >= 2 and mean_silhouette_score[tri-2] < mean_silhouette_score[tri-1] > mean_silhouette_score[tri]):
+            print('refine process ended at iter #{} by stopping criteria'.format(tri-1))
+            break
+
+        # converge criteria
+        if(number_of_eigen0 == P.cluster_size):
+            print('refine process ended at iter #{} by converge criteria'.format(tri))
+            break
+
+        np.savetxt('./result/{}/pred_clusters#{}.csv'.format(P.dataset, tri),
+                   pred_clusters, fmt='%d')
+
+        # update S, lambda, W
         update_S(S, W, eigen_vec)
         update_lamb(lamb, S, sp, Q)
         W = ini_W(sp, lamb)
-    
-#--------------------------------------------------
+
+
+# --------------------------------------------------
 if __name__ == '__main__':
     main()
--- utilities.py
+++ utilities.py
@@ -1,67 +1,76 @@
 import numpy as np
 import math
+from sklearn.metrics import silhouette_samples
 
 from Parameters import P
 import MatLib as ml
-            
+
+
 def load_fromgen(dataset):
     idfeature_features_labels = np.genfromtxt("./data/{0}/{0}.content".format(dataset),
-                                                dtype=np.dtype(str))
-    features = np.array([[float(i) for i in vec] for vec in idfeature_features_labels[:, 1:-1]])
+                                              dtype=np.dtype(str))
+    features = np.array([[float(i) for i in vec]
+                         for vec in idfeature_features_labels[:, 1:-1]])
     labels = idfeature_features_labels[:, -1]
-    clas_map = {clas : l for l, clas in enumerate(set(labels))}
+    clas_map = {clas: l for l, clas in enumerate(set(labels))}
     labels = np.array([l for l in map(clas_map.get, labels)])
     idfeature = np.array(idfeature_features_labels[:, 0], dtype=np.int32)
     idfeature_map = {j: i for i, j in enumerate(idfeature)}
-    edges_unordered = np.genfromtxt("./data/{0}/{0}.cites".format(dataset), dtype=np.int32)
+    edges_unordered = np.genfromtxt(
+        "./data/{0}/{0}.cites".format(dataset), dtype=np.int32)
     edges = np.array(list(map(idfeature_map.get, edges_unordered.flatten())),
-                                    dtype=np.int32).reshape(edges_unordered.shape)
+                     dtype=np.int32).reshape(edges_unordered.shape)
     return features, edges, labels
 
+
 def load_fromcsv(dataset):
-    with open("./data/{0}/{0}_content.csv".format(dataset), 'r') as r:
+    with open("./data/{0}/{0}.content".format(dataset), 'r') as r:
         [dim_size, obj_size] = list(map(int, r.readline().rstrip().split(' ')))
     features = np.zeros((obj_size, dim_size), dtype=np.float)
-    dim_idobjct_val = np.genfromtxt("./data/{0}/{0}_content.csv".format(dataset),
-                                                        skip_header=1, dtype=(int, int, float))
+    dim_idobjct_val = np.genfromtxt("./data/{0}/{0}.content".format(dataset),
+                                    skip_header=1, dtype=(int, int, float))
     for [dim_id, obj_id, val] in dim_idobjct_val:
         features[obj_id][dim_id] = float(val)
-    edges = np.genfromtxt("./data/{0}/{0}_cites.csv".format(dataset),
-                                            skip_header=0, usecols=[1,0], dtype=np.dtype(int))
+    edges = np.genfromtxt("./data/{0}/{0}.cites".format(dataset),
+                          skip_header=0, usecols=[1, 0], dtype=np.dtype(int))
     labels = np.genfromtxt("./data/{0}/{0}_label.csv".format(dataset),
-                                            skip_header=0, dtype=np.dtype(int), delimiter=' ')
+                           skip_header=0, dtype=np.dtype(int), delimiter=' ')
     return features, edges, labels
+
 
 def make_sp1(features):
     def makeMat_sp1_i(feature, usr_size):
         mat = np.zeros((usr_size, usr_size))
         for i in range(usr_size):
             for j in range(i+1, usr_size):
-                mat[i][j] = math.exp(-(feature[i]-feature[j])*(feature[i]-feature[j]) / (2*P.sigma*P.sigma))
+                mat[i][j] = math.exp(-(feature[i]-feature[j])
+                                     * (feature[i]-feature[j]) / (2*P.sigma*P.sigma))
                 mat[j][i] = mat[i][j]
         for d in range(usr_size):
             mat[d][d] = 0.
         return mat
-            
+
     usr_size, dim_size = len(features), len(features[0])
-    if(P.dataset_type == 'social_net'): #AN_type=human
+    if(P.dataset_type == 'social_net'):  # AN_type=human
         mats = [np.zeros((usr_size, usr_size)) for i in range(dim_size)]
         for i, feature in enumerate(features.T):
             mats[i] = makeMat_sp1_i(feature, usr_size)
             ml.normalized(mats[i])
         return mats
-    
+
     elif(P.dataset_type == 'citation_net'):
         mat = np.zeros((usr_size, usr_size))
         for i in range(usr_size):
             for j in range(i+1, usr_size):
-                mat[i][j] = np.dot(features[i], features[j]) / (np.linalg.norm(features[i])                                    * np.linalg.norm(features[j]))
+                mat[i][j] = np.dot(features[i], features[j]) / \
+                    (np.linalg.norm(features[i]) * np.linalg.norm(features[j]))
                 mat[j][i] = mat[i][j]
         for d in range(len(mat)):
             mat[d][d] = 0.
         ml.normalized(mat)
         return [mat]
-            
+
+
 def make_sp2(edges, usr_size):
     gi = Graph(edges, usr_size)
     s2a, s2b = gi.makeMat_sp2()
@@ -69,15 +78,18 @@ def make_sp2(edges, usr_size):
     ml.normalized(s2b)
     return s2a, s2b
 
+
 class Graph():
     def __init__(self, edges, usr_size):
         self.N = np.array([set() for i in range(usr_size)])
         for opponent, obj in edges:
             self.N[obj].add(opponent)
         self.max_size = np.zeros(P.theta+1)
-        self.R = np.array([[set() for i in range(usr_size)] for j in range(P.theta+1)])
-        self.Ft = np.array([[set() for i in range(usr_size)] for j in range(P.theta+1)])    
-    
+        self.R = np.array([[set() for i in range(usr_size)]
+                           for j in range(P.theta+1)])
+        self.Ft = np.array([[set() for i in range(usr_size)]
+                            for j in range(P.theta+1)])
+
     def makeMat_sp2(self):
         s2a = np.diag([0. for i in range(len(self.N))])
         s2b = np.diag([0. for i in range(len(self.N))])
@@ -98,7 +110,8 @@ class Graph():
                 for n in self.N[v]:
                     self.R[step][v] = self.R[step][v] | self.R[step-1][n]
                 self.Ft[step][v] = self.R[step][v].copy() - self.R[step-1][v]
-                self.max_size[step] = max(self.max_size[step], len(self.Ft[step][v]))
+                self.max_size[step] = max(
+                    self.max_size[step], len(self.Ft[step][v]))
                 for v_opp in self.Ft[step][v]:
                     path = 0
                     for v_opp_ft in self.Ft[step-1][v]:
@@ -106,7 +119,7 @@ class Graph():
                             path += 1
                     s2a[v][v_opp], s2b[v][v_opp] = self.calc(step, path, v)
         return s2a, s2b
-    
+
     def calc(self, step, path, v):
         if(len(self.Ft[step-1][v]) <= 1):
             rate1 = 0
@@ -115,6 +128,7 @@ class Graph():
         rate2 = (path-1) / self.max_size[step-1]
         delta = math.pow(P.delta, step-1) - math.pow(P.delta, step)
         return math.pow(P.delta, step) + delta * rate1, math.pow(P.delta, step) + delta * rate2
+
 
 def purity(ans, estimated):
     usr_size = len(estimated)
@@ -127,3 +141,37 @@ def purity(ans, estimated):
     for k in range(len(table)):
         sum += np.amax(table[k])
     return sum/usr_size
+
+
+def calc_silhouette_score(dataset, pred_clusters):
+    if(dataset == 'cora'):
+        idfeature_features_labels = np.genfromtxt("./data/{0}/{0}.content".format(dataset),
+                                                  dtype=np.dtype(str))
+        features = np.array([[float(i) for i in vec]
+                             for vec in idfeature_features_labels[:, 1:-1]])
+
+    if(dataset == 'citeseer'):
+        with open("./data/{0}/{0}.content".format(dataset), 'r') as r:
+            [dim_size, obj_size] = list(
+                map(int, r.readline().rstrip().split(' ')))
+        features = np.zeros((obj_size, dim_size), dtype=np.float)
+        dim_idobjct_val = np.genfromtxt("./data/{0}/{0}.content".format(dataset),
+                                        skip_header=1, dtype=(int, int, float))
+        for [dim_id, obj_id, val] in dim_idobjct_val:
+            features[obj_id][dim_id] = float(val)
+
+    if(dataset in ['football', 'politicsuk', 'olympics']):
+        with open("./data/{0}/{0}_tfidf.content".format(dataset), 'r') as r:
+            [dim_size, obj_size] = list(
+                map(int, r.readline().rstrip().split(' ')))
+        features = np.zeros((obj_size, dim_size), dtype=np.float)
+        dim_idobjct_val = np.genfromtxt("./data/{0}/{0}_tfidf.content".format(dataset),
+                                        skip_header=1, dtype=(int, int, float))
+        for [dim_id, obj_id, val] in dim_idobjct_val:
+            features[obj_id][dim_id] = float(val)
+
+    silhouette_scores = silhouette_samples(
+        features, pred_clusters, metric='cosine')
+    mean_silhouette_score = np.mean(silhouette_scores)
+
+    return mean_silhouette_score
